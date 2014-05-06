866661cbe2f7c9ff5123981897095ce50caeccd4
Jerome Botineau <jeromebotineau@gmail.com>
2014-05-06 23:05:41 +0200

    add test / mocha

---
+++ test/index.js
@@ -0,0 +1,26 @@
+var nock = require('nock'),
+  vlc = require('../')();
+
+vlc._base = 'http://vlc-api.test';
+
+var assert = require("assert");
+describe('vlc-api', function(){
+  it('should change to next without error', function(done){
+    var scope = nock('http://vlc-api.test')
+      .get('/requests/status.json?command=pl_next')
+      .reply(200);
+    vlc.status.next(done);
+  })
+  it('should change to previous without error', function(done){
+    var scope = nock('http://vlc-api.test')
+      .get('/requests/status.json?command=pl_previous')
+      .reply(200);
+    vlc.status.previous(done);
+  })
+  it('should pause without error', function(done){
+    var scope = nock('http://vlc-api.test')
+      .get('/requests/status.json?command=pl_pause')
+      .reply(200);
+    vlc.status.pause(done);
+  })
+})
